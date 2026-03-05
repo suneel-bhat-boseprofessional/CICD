51255f441138020156b7ab6e3cbc864940a31c13
Sudhanand-Dayalan-BoseProfessional <sudhanand.dayalan@boseprofessional.com>
2026-03-05 17:18:40 -0500

    Updated LTDC clock divider for new clock settings. Modified LTDC clock polarity to inverted.

--- Appli/Core/Src/main.c
+++ Appli/Core/Src/main.c
@@ -1061,7 +1061,8 @@ static void MX_LTDC_Init(void)
   hltdc.Init.HSPolarity = LTDC_HSPOLARITY_AL;
   hltdc.Init.VSPolarity = LTDC_VSPOLARITY_AL;
   hltdc.Init.DEPolarity = LTDC_DEPOLARITY_AL;
-  hltdc.Init.PCPolarity = LTDC_PCPOLARITY_IPC;
+  //hltdc.Init.PCPolarity = LTDC_PCPOLARITY_IPC;
+  hltdc.Init.PCPolarity = LTDC_PCPOLARITY_IIPC;
 
   ////////////////////////////////////////////////////
   // QUESTION: The VSYNC Display period in the display datat sheet shows 272, should this be 128????
