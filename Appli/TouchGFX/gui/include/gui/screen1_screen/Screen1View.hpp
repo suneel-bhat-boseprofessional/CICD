42f925553b493874157db7634057eb046caefd58
Pushparaj Adka <pushparaj.adka@boseprofessional.com>
2026-05-07 20:35:36 +0530

    reduced number of screens to match ggec dev

--- Appli/TouchGFX/gui/include/gui/screen1_screen/Screen1View.hpp
+++ Appli/TouchGFX/gui/include/gui/screen1_screen/Screen1View.hpp
@@ -11,7 +11,10 @@ public:
     virtual ~Screen1View() {}
     virtual void setupScreen();
     virtual void tearDownScreen();
+
+    void changeScreenColor();
 protected:
+    uint8_t currentColorState;
 };
 
 #endif // SCREEN1VIEW_HPP
--- Appli/TouchGFX/gui/src/screen1_screen/Screen1View.cpp
+++ Appli/TouchGFX/gui/src/screen1_screen/Screen1View.cpp
@@ -1,6 +1,8 @@
 #include <gui/screen1_screen/Screen1View.hpp>
+#include <touchgfx/Color.hpp>
 
 Screen1View::Screen1View()
+    : currentColorState(0)
 {
 
 }
@@ -8,9 +10,61 @@ Screen1View::Screen1View()
 void Screen1View::setupScreen()
 {
     Screen1ViewBase::setupScreen();
+    // Ensure button is touchable even with alpha=0
+    imageChangeBtn.setTouchable(true);
 }
 
 void Screen1View::tearDownScreen()
 {
     Screen1ViewBase::tearDownScreen();
+}
+
+
+void Screen1View::changeScreenColor()
+{
+    // Cycle through colors: Red -> Blue -> Green -> White -> Black -> Transparent
+    switch(currentColorState)
+    {
+        case 0: // Red
+            colorImage.setColor(touchgfx::Color::getColorFromRGB(255, 0, 0));
+            colorImage.setAlpha(255);
+            break;
+
+        case 1: // Blue
+            colorImage.setColor(touchgfx::Color::getColorFromRGB(0, 0, 255));
+            colorImage.setAlpha(255);
+            break;
+
+        case 2: // Green
+            colorImage.setColor(touchgfx::Color::getColorFromRGB(0, 255, 0));
+            colorImage.setAlpha(255);
+            break;
+
+        case 3: // White
+            colorImage.setColor(touchgfx::Color::getColorFromRGB(255, 255, 255));
+            colorImage.setAlpha(255);
+            break;
+
+        case 4: // Black
+            colorImage.setColor(touchgfx::Color::getColorFromRGB(0, 0, 0));
+            colorImage.setAlpha(255);
+            break;
+
+        case 5: // Transparent (0 intensity)
+            colorImage.setAlpha(0);
+            break;
+
+        default:
+            break;
+    }
+
+    // Invalidate to trigger redraw
+    colorImage.invalidate();
+
+    // Move to next color state
+    currentColorState++;
+    if(currentColorState > 5)
+    {
+        currentColorState = 0;
+    }
 }
