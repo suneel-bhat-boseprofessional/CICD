4f1622475790ee43f43132bc19c578e6df6ca2d3
Suneel Bhat <suneel.bhat@boseprofessional.com>
2026-05-08 14:37:45 +0530

    Sendng 'readyAck' on startup

--- Appli/Core/Src/protocol.c
+++ Appli/Core/Src/protocol.c
@@ -493,6 +493,9 @@ void FanControl_Init(TIM_HandleTypeDef *htim, UART_HandleTypeDef *huart, uint32_
   // Set initial fan speed to LOW
   FanControl_SetSpeed(SPEED_LOW_PERCENT);
 
+  // Send readyAck on powerup, same as when we receive a ready message
+  const char *ack = "{\"action\":\"readyAck\"}";
+  Protocol_SendFramed(ack, strlen(ack));
 }
 
 /**
